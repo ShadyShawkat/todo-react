a28c1de5c73f98cf64cc237bb3e63f0717aa6a64
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-10 20:17:42 +0200

    Finish basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import styles from "./App.module.css";
+import { useState } from 'react';
+import styles from './App.module.css';
+
+import TodoList from './components/TodoList/TodoList';
+
+const todoDummyData = [
+  {
+    id: 1,
+    text: 'Walk the dog',
+    completed: true,
+  },
+  {
+    id: 2,
+    text: 'DO homework',
+    completed: false,
+  },
+  {
+    id: 3,
+    text: 'work on project',
+    completed: true,
+  },
+];
 
 const App = () => {
-  return <div>Hello</div>;
+  const [todoList, setTodoList] = useState(todoDummyData);
+  console.log(setTodoList);
+
+  return (
+    <div className={styles.wrapper}>
+      <TodoList todoList={todoList} />
+    </div>
+  );
 };
 
 export default App;
