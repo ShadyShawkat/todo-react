ad9ccce1baf326d4d80136ff65ca32c27c44a073
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-11 02:03:22 +0200

    Finish basic styling

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,22 @@
+/* eslint-disable object-curly-newline */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoItem.module.css';
 
 const TodoItem = ({
-  id, text, completed, changeTodoState,
+  id,
+  text,
+  completed,
+  changeTodoState,
+  deleteTodo,
+  editTodo,
 }) => (
-  <li className={styles['todo-item']}>
+  <li
+    className={`${styles['todo-item']} ${
+      completed && styles['todo-completed']
+    }`}
+  >
     <input
       type="checkbox"
       id={id}
@@ -15,6 +26,20 @@ const TodoItem = ({
       onChange={() => changeTodoState(id)}
     />
     <input type="text" disabled value={text} className={styles['todo-text']} />
+    <button
+      className={styles['delete-btn']}
+      type="button"
+      onClick={() => deleteTodo(id)}
+    >
+      <i className="fas fa-trash-alt" />
+    </button>
+    <button
+      className={styles['edit-btn']}
+      type="button"
+      onClick={() => editTodo(id)}
+    >
+      <i className="fas fa-pen" />
+    </button>
   </li>
 );
 
@@ -23,6 +48,8 @@ TodoItem.propTypes = {
   text: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
   changeTodoState: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,26 +1,36 @@
+/* eslint-disable object-curly-newline */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoList.module.css';
 
 import TodoItem from '../TodoItem/TodoItem';
 
-const TodoList = ({ todoList, changeTodoState }) => (
-  <ul className={styles['todo-list']}>
-    {todoList.map((todoItem) => (
-      <TodoItem
-        key={todoItem.id}
-        id={todoItem.id}
-        text={todoItem.text}
-        completed={todoItem.completed}
-        changeTodoState={changeTodoState}
-      />
-    ))}
-  </ul>
+const TodoList = ({ todoList, changeTodoState, deleteTodo, editTodo }) => (
+  <>
+    {todoList.length && (
+      <ul className={styles['todo-list']}>
+        {todoList.map((todoItem) => (
+          <TodoItem
+            key={todoItem.id}
+            id={todoItem.id}
+            text={todoItem.text}
+            completed={todoItem.completed}
+            changeTodoState={changeTodoState}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        ))}
+      </ul>
+    )}
+  </>
 );
 
 TodoList.propTypes = {
   todoList: PropTypes.arrayOf(TodoItem).isRequired,
   changeTodoState: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
 };
 
 export default TodoList;
